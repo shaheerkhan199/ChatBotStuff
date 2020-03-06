add5233ac4286d63973f967c3b0f996505d9aac9
Muhammad Shaheer Khan <m.shaheerkhan199@gmail.com>
2020-03-06 02:13:46 -0800

    Refactor from previous version

--- conversation data/big conversation data from api/datapreprocessing.py
+++ conversation data/big conversation data from api/datapreprocessing.py
@@ -5,25 +5,31 @@ from chatterbot.trainers import ListTrainer
 my_bot = ChatBot("Smart Bot")
 trainer = ListTrainer(my_bot)
 
+def extract_data(api):
+    raw_data = requests.get(api).json()
+    useable_conversation = list()
+    for conv in raw_data:
+        lis_conv = conv['dialog']
+        for i in range(len(lis_conv)):
+            #print(lis_conv[i]['text'])
+            useable_conversation.append(lis_conv[i]['text'])
+    return useable_conversation
 
-raw_data = requests.get('https://raw.githubusercontent.com/DeepPavlov/convai/master/data/data_volunteers.json').json()
+
 #another_api = "https://raw.githubusercontent.com/DeepPavlov/convai/master/data/summer_wild_evaluation_dialogs.json"
-# print(raw_data[8]['dialog'])
-#print("***************Test****************")
-# chat1 = raw_data[8]['dialog']
-# useable_conversation = list()
-# for conv in raw_data[:2]:
-#     for i in range(len(conv['dialog'])):
-#         print(conv[i]['text'])
-useable_conversation = list()
-for conv in raw_data:
-    lis_conv = conv['dialog']
-    for i in range(len(lis_conv)):
-        #print(lis_conv[i]['text'])
-        useable_conversation.append(lis_conv[i]['text'])
-        
-#     print("New")
-print(useable_conversation[])
+
+#useable_conversation = list()
+#for conv in raw_data:
+#    lis_conv = conv['dialog']
+#    for i in range(len(lis_conv)):
+#        #print(lis_conv[i]['text'])
+#        useable_conversation.append(lis_conv[i]['text'])
+ 
+conversation1 = extract_data('https://raw.githubusercontent.com/DeepPavlov/convai/master/data/data_volunteers.json')
+print(conversation1[:20])
+
+conversation2 = extract_data('https://raw.githubusercontent.com/DeepPavlov/convai/master/data/summer_wild_evaluation_dialogs.json')
+print(conversation2[:20])
 #trainer.train(useable_conversation)
 #while True:
 #    user_input = input("You:")
