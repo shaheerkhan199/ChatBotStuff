328b1ce1076161710e25628f0d0e01fc034bf982
Muhammad Shaheer Khan <m.shaheerkhan199@gmail.com>
2020-09-22 01:27:14 -0800

    Integrating chatbot with UI

--- Ecommerce website/EcommerceChatBot/Shop/shopping_bot.py
+++ Ecommerce website/EcommerceChatBot/Shop/shopping_bot.py
@@ -1,26 +1,34 @@
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 import random
 myBot = ChatBot(
     'Shopping Assistant',
     preprocessors=['chatterbot.preprocessors.clean_whitespace'],
     storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    # logic_adapters=[
-    #     {
-    #         'import_path': 'priceAskingLogicAdapter.PriceAskingLogicAdapter'
-    #     },
-    #     {
-    #         'import_path': 'negotiationLogicAdapter.NegotiationLogicAdapter'
-    #     },
-    #     {
-    #         'import_path': 'negotiationSuccessLogicAdapter.NegotiationSuccessLogicAdapter'
-    #     },
-    #     'chatterbot.logic.BestMatch',
+    logic_adapters=[
+        # {
+        #     'import_path': 'priceAskingLogicAdapter.PriceAskingLogicAdapter'
+        # },
+        # {
+        #     'import_path': 'negotiationLogicAdapter.NegotiationLogicAdapter'
+        # },
+        # {
+        #     'import_path': 'negotiationSuccessLogicAdapter.NegotiationSuccessLogicAdapter'
+        # },
+        'chatterbot.logic.BestMatch',
         
-    # ],
+    ],
 )
-trainer = ChatterBotCorpusTrainer(myBot)
-trainer.train()
+trainer = ListTrainer(myBot)
+conversation = [
+    "Hello",
+    "Hey man! How may i assist you?",
+    "Who are you?",
+    "I am your shopping assistant",
+    "Can you help me?",
+    "yes i am here to help you",
+]
+trainer.train(conversation)
 # print("I am chatbot! you can talk to me")
 # greetings = ['Hi', 'Hello', 'Hey', 'What\'s up?']
 # print(random.choice(greetings) + " This is Shopping Assistant! How may i help you?")
